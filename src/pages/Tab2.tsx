2f8662659e25195505e220659a052ed18cbb6473
User <revina8462@gmail.com>
2024-04-25 13:00:29 +0700

    Ionic Kamera

---
+++ src/pages/Tab2.tsx
@@ -0,0 +1,78 @@
+import ExploreContainer from '../components/ExploreContainer';
+import './Tab2.css';
+
+import { camera, trash, close } from 'ionicons/icons';
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonFab,
+  IonFabButton,
+  IonIcon,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonActionSheet,
+} from '@ionic/react';
+import React, { useState } from 'react';
+import { UserPhoto, usePhotoGallery } from '../hooks/usephotogallery';
+
+const Tab2: React.FC = () => {
+  const { photos,takePhoto, deletePhoto } = usePhotoGallery();
+  const [photoToDelete, setPhotoToDelete] = useState<UserPhoto>();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>photo-gallery Revina febriani</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+
+        <IonGrid>
+          <IonRow>
+            {photos.map((photo, index) => (
+              <IonCol size="6" key={photo.filepath}>
+                <IonImg onClick={() => setPhotoToDelete(photo)} src={photo.webviewPath} />
+                <IonImg src={photo.webviewPath} />
+              </IonCol>
+            ))}
+          </IonRow>
+        </IonGrid>
+  <IonFab vertical="bottom" horizontal="center" slot="fixed">
+    <IonFabButton onClick={() => takePhoto()}>
+      <IonIcon icon={camera}></IonIcon>
+    </IonFabButton>
+  </IonFab>
+<IonActionSheet
+isOpen={!!photoToDelete}
+buttons={[
+  {
+    text: 'Delete',
+    role: 'destructive',
+    icon: trash,
+    handler: () => {
+      if (photoToDelete) {
+        deletePhoto(photoToDelete);
+        setPhotoToDelete(undefined);
+      }
+    },
+  },
+  {
+    text: 'Cancel',
+    icon: close,
+    role: 'cancel',
+  },
+]}
+onDidDismiss={() => setPhotoToDelete(undefined)}
+/>
+</IonContent>
+    </IonPage>
+  );
+};
+
+export default Tab2;
